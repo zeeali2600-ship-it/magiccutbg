107bb5e9603b696afb95df91a5841039c9115b45
zeeali2600-ship-it <zee.ali2600@gmail.com>
2025-11-10 11:23:57 -0300

    Update service worker cache version and asset paths

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,31 @@
-const CACHE_NAME = 'magiccutbg-v1';
+const CACHE_NAME = 'magiccutbg-v2';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/privacy.html',
-  '/design.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './privacy.html',
+  './design.png'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', event => {
-  const url = new URL(event.request.url);
   if (event.request.mode === 'navigate') {
-    event.respondWith(
-      fetch(event.request).catch(() => caches.match('/index.html'))
-    );
+    event.respondWith(fetch(event.request).catch(() => caches.match('./index.html')));
     return;
   }
   event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request).catch(()=> caches.match('/index.html')))
+    caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
